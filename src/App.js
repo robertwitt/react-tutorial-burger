ba15f12fc4eda9fd8b9e14d54e0232d62a66cc7c
Robert Witt <robert.witt@zalando.de>
2019-03-18 20:06:19 +0100

    Add empty BurgerBuilder component and incorporate it into the app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
+
   render() {
     return (
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder/>
       </Layout>
     );
   }
+
 }
 
 export default App;
