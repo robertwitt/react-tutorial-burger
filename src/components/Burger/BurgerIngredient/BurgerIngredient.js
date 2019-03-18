0e60d472c530e0e018a964480267b2b2b115a348
Robert Witt <robert.witt@zalando.de>
2019-03-18 20:34:46 +0100

    Start draft of Burger component and include it into BurgerBuilder.

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,5 @@
 import React, {  Component } from 'react';
-import classes from './burgerIngredient';
+import classes from './BurgerIngredient.css';
 import PropTypes from 'prop-types';
 
 class BurgerIngredient extends Component {
