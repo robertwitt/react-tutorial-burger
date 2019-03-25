6753259fed184e5eef649d385ee3797811d1c876
Robert Witt <robert.witt@zalando.de>
2019-03-25 13:00:14 +0100

    Performance improvement in Modal component.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map(ingredientKey => (
-      <li key={ingredientKey}>
-        <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {props.ingredients[ingredientKey]}
-      </li>
+class OrderSummary extends Component {
+
+  componentDidUpdate() {
+    console.log('[OrderSummary] did update')
+  }
+
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map(ingredientKey => (
+        <li key={ingredientKey}>
+          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>: {this.props.ingredients[ingredientKey]}
+        </li>
+      )
+    );
+
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button buttonType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+        <Button buttonType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
     )
-  );
+  }
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button buttonType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-      <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
-};
+}
 
-export default orderSummary;
+export default OrderSummary;
