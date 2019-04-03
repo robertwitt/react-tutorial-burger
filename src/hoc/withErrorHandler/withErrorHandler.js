7a66e5050ca3aad33d94d730a7f39da74088b691
Robert Witt <robert.witt@zalando.de>
2019-04-03 09:14:24 +0200

    Remove interceptors im error handler.

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -9,15 +9,19 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     }
 
-    constructor(props) {
-      super(props);
-      axios.interceptors.request.use(request => {
+    componentWillMount() {
+      this.requestInterceptor = axios.interceptors.request.use(request => {
         this.setState({ error: null });
         return request;
       })
-      axios.interceptors.response.use(response => response, error => {
+      this.responseInterceptor = axios.interceptors.response.use(response => response, error => {
         this.setState({ error: error });
       });
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     errorConfirmedHandler = () => {
